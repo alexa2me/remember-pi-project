eaf374b67652679d1ca0408266826bd3e7639f90
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2023-07-01 22:41:47 -0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import './App.css';
+import './styles/App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import Login from "./pages/login/Login"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Login />
+    </ChakraProvider>
   );
 }
 
