1682a9d3fd263a67e7dc53152ba2e2a1330255d3
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2023-07-15 21:00:09 -0300

    Create form and access hooks

---
+++ src/hooks/useUnprotectedPage.js
@@ -0,0 +1,16 @@
+import { useLayoutEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+// import { goToHome } from '../routes/coordinator';
+
+const useUnprotectedPage = () => {
+    const navigate = useNavigate();
+
+    useLayoutEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            // goToHome(navigate);
+        }
+    }, [navigate]);
+};
+
+export default useUnprotectedPage;
