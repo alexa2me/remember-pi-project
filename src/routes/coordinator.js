2362be8d650ee76e709f724c94b5422477675cd7
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2023-07-15 21:00:34 -0300

    Create routes

---
+++ src/routes/coordinator.js
@@ -0,0 +1,11 @@
+export const goToLogin = (navigate) => {
+    navigate('/');
+  };
+  
+  export const goToSignUp = (navigate) => {
+    navigate('/cadastro');
+  };
+
+  export const goToHome = (navigate) => {
+    navigate('/home');
+  };
