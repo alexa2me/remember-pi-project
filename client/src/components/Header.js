492a19abdb7cdeb24a74ff8d6367334cd51d7b5b
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2023-08-11 11:33:31 -0300

    Update Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,13 @@
-import { goToLogin } from "../routes/coordinator";
-import { useNavigate } from 'react-router-dom';
+import { goToHome, goToLogin, goToProfile } from "../routes/coordinator";
+import { useNavigate, useLocation } from 'react-router-dom';
 import logo from "../images/remember-icon.png"
 import { Grid, GridItem, Text } from "@chakra-ui/react";
 
 const Header = () => {
   const token = localStorage.getItem("token");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isProfilePage = location.pathname === "/profile";
 
   const logout = () => {
     if (token) {
@@ -22,14 +24,16 @@ const Header = () => {
         backgroundColor={"#DACAE8"}
         padding={{ lg: '20px 100px', md:'20px 40px', sm: '20px 20px' }}
         alignItems={"center"}
+        fontFamily='roboto'
     >
-        <GridItem width={"100px"}>
+        <GridItem width={"100px"} onClick={() => goToHome(navigate)}>
             <img src={logo} alt="Logo" className="logo-header"/>
         </GridItem>
         <Text
             color={"#FFFFFF"}
             textShadow={"3px 3px 3px #9C9C9C"}
             fontSize={{ base: '30px', lg: '40px'}}
+            onClick={() => goToHome(navigate)}
         >
             Remember
         </Text>
@@ -39,7 +43,9 @@ const Header = () => {
             cursor={"pointer"}
             fontSize={22}
             width={"fit-content"}
-            _hover={{ textDecoration: "underline" }}
+            _hover={{ textDecoration: isProfilePage ? "none" : "underline" }}
+            textDecoration={isProfilePage ? "underline" : "none"}
+            onClick={() => goToProfile(navigate)}
             justifySelf={"end"}
         >
             Perfil
