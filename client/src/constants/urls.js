dc2cdae7acda24eb6255d906e14b7ec7278173b2
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2023-09-16 09:15:59 -0300

    fix url path

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,4 +1,4 @@
-// const BASE_URL = "https://remember-pi-project-alexa2me.vercel.app";
-const BASE_URL =  "http://localhost:3306";
+const BASE_URL = "https://remember-pi-project-alexa2me.vercel.app";
+// const BASE_URL =  "http://localhost:3306";
 
 export default BASE_URL;
