8ddd197475220b884fb4b66b33b002d851a46b93
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2023-09-16 13:32:07 -0300

    Update queries

--- api/src/data/postQueries.ts
+++ api/src/data/postQueries.ts
@@ -20,20 +20,6 @@ export const getPosts = async (id: string): Promise<any> => {
   return result;
 };
 
-export const getPostById = async (id: string): Promise<any> => {
-  const result = await connection("posts")
-    .select(
-      "posts.id",
-      "post_title",
-      "post_content",
-      "user_id",
-      "created_at"
-    )
-    .where("posts.id", `${id}`);
-
-  return result;
-};
-
 export const deletePost = async (id: string): Promise<any> => {
   await connection("posts").where("id", id).delete();
 };
@@ -46,6 +32,10 @@ export const updatePost = async (postId: string, newTitle: string, newContent: s
     })
     .where("id", postId)
 };
+
+export const deleteAllPostsByUserId = async (id: string): Promise<void> => {
+  await connection("posts").where("user_id", id).delete();
+}
 
 
 
